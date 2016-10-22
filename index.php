433d06abacd97c47c40fda18f76d38d77cf30a7d
Bob Sforza <rpsforza@gmail.com>
2016-10-21 20:24:19 -0500

    add db

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+	$server = "us-cdbr-iron-east-04.cleardb.net";
+	$username = "b50c03721510b5";
+	$password = "15258fda";
+	$db = "heroku_0e49192e673a1d3";
+
+	$mysqli = new mysqli($server, $username, $password, $db);
+?>
