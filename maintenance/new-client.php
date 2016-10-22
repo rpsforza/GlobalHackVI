b712e80c5ff8d7a077c50082db08766410095a81
Michel Ge <strengthofthepen@gmail.com>
2016-10-21 23:50:56 -0500

    new clinet

--- maintenance/new-client.php
+++ maintenance/new-client.php
@@ -4,8 +4,24 @@ require("../db_manager.php");
 
 if (isset($_POST)) {
 	
-	
-	
+	$first_name = $_POST["first_name"];
+	$middle_name = $_POST["middle_name"];
+	$last_name = $_POST["last_name"];
+	$dob = $_POST["dob"];
+	$gender = $_POST["gender"];
+
+	// $date_created = current date;
+
+	// TODO make UUID auto increment
+
+	$mysqli = getDB();
+
+	$statement = $mysqli->prepare("INSERT INTO client (First_Name, Middle_Name, Last_Name, DOB, Gender)
+									VALUES (?, ?, ?, ?, ?)");
+	$statement->bind_param("ssssi", $first_name, $middle_name, $last_name, $dob, $gender);
+	$statement->execute();
+
+
 } else {
 	echo "POST data not set.";
 }
