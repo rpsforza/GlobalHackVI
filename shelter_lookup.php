ab8870c702ce246662d5dfcef58cc8c50746d843
Michel Ge <strengthofthepen@gmail.com>
2016-10-22 01:44:35 -0500

    shelter lookup complete

--- shelter_lookup.php
+++ shelter_lookup.php
@@ -2,6 +2,13 @@
 	
 	require("db_manager.php");
 
+	function getAllShelters() {
+		$mysqli = getDB();
+		$cocs = $mysqli->query("SELECT * FROM coc WHERE is_shelter=1")->fetch_all(MYSQLI_ASSOC);
+		$hosts = $mysqli->query("SELECT * FROM host")->fetch_all(MYSQLI_ASSOC);
+		return array_merge($cocs, $hosts);
+	}
+
 	function getCompatibleShelters($client_id) {
 		$mysqli = getDB();
 		$client = $myqli->query("SELECT * FROM clients WHERE id=$client_id")->fetch_assoc();
@@ -19,6 +26,29 @@
 		$client_male = $client["Gender"] === 1 ? 1 : 0;
 		$client_female = $client["Gender"] === 0 ? 1 : 0;
 		$client_veteran = $client["VeteranStatus"] === 1 ? 1 : 0;
+
+		$shelters = getAllShelters();
+		$compatible_shelters = [];
+
+		for ($i = 0; $i < count($shelters); $i++) {
+			$shelter = $shelters[$i];
+			$minage = $shelter["condition_minage"];
+			$maxage = $shelter["condition_maxage"];
+			$male = $shelter["condition_male"];
+			$female = $shelter["condition_female"];
+			$abuse = $shelter["condition_abuse"];
+			$veteran = $shelter["condition_veteran"];
+
+			// doesn't add this shelter if the conditions aren't met
+			if ($client_age < $minage || $client_age > $maxage) continue;
+			if (($male && !$client_male) || ($female && !$client_female)) continue;
+			if ($abuse && !$client_abuse) continue;
+			if ($veteran && !$client_veteran) continue;
+
+			$compatible_shelters[] = $shelter;
+		}
+
+		return $compatible_shelters;
 	}
 
 ?>
