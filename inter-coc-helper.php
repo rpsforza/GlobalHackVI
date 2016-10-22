5a3d03e66afe881b819ec027d8131cd8b060b63f
iProdigy <prodigycreep@gmail.com>
2016-10-22 11:46:17 -0500

    fix inter-coc-helper.php

--- inter-coc-helper.php
+++ inter-coc-helper.php
@@ -1,30 +1,27 @@
 <?php
 
-	require("../db_manager.php");
+require("db_manager.php");
 
-	// $identifier is either "id" or "name"
-	function getServicesListByIdentifier($identifier) {
+// $identifier is either "id" or "name"
+function getServicesListByIdentifier($identifier)
+{
+	$mysqli = getDB();
+	$services_raw = $mysqli->query("SELECT * FROM services")->fetch_all(MYSQLI_ASSOC);
+	return array_column($services_raw, $identifier);
+}
 
-		$mysqli->getDB();
-		$services_raw = $mysqli->query("SELECT * FROM services")->fetch_all(MYSQLI_ASSOC);
-		return array_column($services_raw, $identifier);
+function getServicesProvided($client_id)
+{
+	$mysqli = getDB();
+	$service_ids = getServicesListByIdentifier("id");
+	$services_provided = [];
 
-	}
-
-	function getServicesProvided($client_id) {
-
-		$mysqli->getDB();
-		$service_ids = getServicesListByIdentifier("id");
-		$services_provided = [];
-
-		foreach ($service_ids as $service_id) {
-			$service_record = $mysqli->query("SELECT * FROM provided_services WHERE client_id=$client_id AND service_id=$service_id");
-			if ($service_record) {
-				$services_provided[] = $service_record;
-			}
+	foreach ($service_ids as $service_id) {
+		$service_record = $mysqli->query("SELECT * FROM provided_services WHERE client_id=$client_id AND service_id=$service_id");
+		if ($service_record) {
+			$services_provided[] = $service_record;
 		}
-
-		return $services_provided;
 	}
 
-?>
+	return $services_provided;
+}
