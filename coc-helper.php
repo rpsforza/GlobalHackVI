273cdeff98d38c874a008beab585c557b1327018
Michel Ge <strengthofthepen@gmail.com>
2016-10-22 12:34:00 -0500

    tracking clients moving out

--- coc-helper.php
+++ coc-helper.php
@@ -26,3 +26,26 @@ function getServicesProvided($client_id)
 	return $services_provided;
 }
 
+function newServiceReport($client_id, $service_id, $host_or_coc, $provider_id, $completed, $comments) {
+	$mysqli = getDB();
+
+	$date = date("m/d/y");
+
+	$statement = $mysqli->prepare("INSERT INTO provided_services (client_id, service_id, host_or_coc, provider_id, completed, comments, date) VALUES (?,?,?,?,?,?)");
+	$statement->bind_param("iisiiss", $client_id, $service_id, $host_or_coc, $provider_id, $completed, $comments, $date);
+	$statement->execute();
+}
+
+// report a client moving out of temporary shelter and into their own housing / a permanent housing service
+// $rehousing_or_permanent_housing is either "rehousing" or "permanent_housing"
+function clientMoveOut($client_id, $rehousing_or_permanent_housing, $new_address) {
+	$mysqli = getDB();
+
+	$date = date("m/d/y");
+
+	$statement = $mysqli->prepare("INSERT INTO shelter_outputs (date, client_id, rehousing_or_permanent_housing, home_address) VALUES (?,?)");
+	$statement->bind_param("siss", $date, $client_id, $rehousing_or_permanent_housing, $new_address);
+	$statement->execute();
+
+	$mysqli->query("UPDATE client SET moved_on=1 WHERE id=$client_id");
+}
