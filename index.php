48766ae60b85879ef65329733ec6ee3054fd639e
iProdigy <prodigycreep@gmail.com>
2016-10-21 20:28:38 -0500

    formaterino 1

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-	$server = "us-cdbr-iron-east-04.cleardb.net";
-	$username = "b50c03721510b5";
-	$password = "15258fda";
-	$db = "heroku_0e49192e673a1d3";
+$server = "us-cdbr-iron-east-04.cleardb.net";
+$username = "b50c03721510b5";
+$password = "15258fda";
+$db = "heroku_0e49192e673a1d3";
 
-	$mysqli = new mysqli($server, $username, $password, $db);
-?>
+$mysqli = new mysqli($server, $username, $password, $db);
