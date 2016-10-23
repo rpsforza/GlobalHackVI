bcd0cf034c5faa50470f841150fa2ea20bfa3c8d
Michel Ge <strengthofthepen@gmail.com>
2016-10-23 00:30:30 -0500

    fix

--- search.php
+++ search.php
@@ -16,8 +16,7 @@
 
 			for ($i = 0; $i < count($search_terms); $i++) {
 				$term = $search_terms[$i];
-				$new_results = $mysqli->query("SELECT * FROM client WHERE First_Name='$term' OR Middle_Name='$term' OR Last_Name='$term'") or die($mysqli->error);
-				$new_results = $new_results->fetch_all(MYSQLI_ASSOC);
+				$new_results = $mysqli->query("SELECT * FROM client WHERE First_Name='$term' OR Middle_Name='$term' OR Last_Name='$term'")->fetch_all(MYSQLI_ASSOC);
 				$results = array_unique(array_merge($results, $new_results), SORT_REGULAR);
 			}
 
@@ -25,14 +24,15 @@
 
 			for ($i = 0; $i < count($search_terms); $i++) {
 				$term = $search_terms[$i];
-				$new_results = $mysqli->query("SELECT * FROM coc WHERE name=$'term'")->fetch_all(MYSQLI_ASSOC);
+				$new_results = $mysqli->query("SELECT * FROM coc WHERE name='$term'")->fetch_all(MYSQLI_ASSOC);
 				$results = array_unique(array_merge($results, $new_results), SORT_REGULAR);
-				$new_results = $mysqli->query("SELECT * FROM host WHERE name=$'term'")->fetch_all(MYSQLI_ASSOC);
+				$new_results = $mysqli->query("SELECT * FROM host WHERE name='$term'")->fetch_all(MYSQLI_ASSOC);
 				$results = array_unique(array_merge($results, $new_results), SORT_REGULAR);
 			}
 		}
-
-		echo var_dump($results);
+		echo "<pre>";
+		var_dump($results);
+		echo "</pre>";
 		echo "HI";
 	}
 ?>
