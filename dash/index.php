ab4c156e8e19730c737285a2ed816718f12a5afb
Michel Ge <strengthofthepen@gmail.com>
2016-10-23 04:49:04 -0500

    delete old search

--- dash/index.php
+++ dash/index.php
@@ -173,32 +173,5 @@ require '../search.php';
 		</main>
 	</div>
 	<script src="https://code.getmdl.io/1.1.3/material.min.js"></script>
-	<script type="text/javascript">
-		$(document).ready(function() {
-		  $('.mdh-toggle-search').click(function() {
-		    // No search bar is currently shown
-		    if ($(this).find('i').text() == 'search') {
-		      $(this).find('i').text('cancel');
-		      $(this).removeClass('mdl-cell--hide-tablet mdl-cell--hide-desktop'); // Ensures the close button doesn't disappear if the screen is resized.
-
-		      $('.mdl-layout__drawer-button, .mdl-layout-title, .mdl-badge, .mdl-layout-spacer').hide();
-		      $('.mdl-layout__header-row').css('padding-left', '16px'); // Remove margin that used to hold the menu button
-		      $('.mdh-expandable-search').removeClass('mdl-cell--hide-phone').css('margin', '0 16px 0 0');
-		      
-		    }
-		    // Search bar is currently showing
-		    else {
-		      $(this).find('i').text('search');
-		      $(this).addClass('mdl-cell--hide-tablet mdl-cell--hide-desktop');
-		      
-		      $('.mdl-layout__drawer-button, .mdl-layout-title, .mdl-badge, .mdl-layout-spacer').show();
-		      $('.mdl-layout__header-row').css('padding-left', '');
-		      $('.mdh-expandable-search').addClass('mdl-cell--hide-phone').css('margin', '0 50px');
-		    }
-		    
-		  });
-		});
-
-	</script>
 </body>
 </html>
