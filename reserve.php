7b25ca31be7c216121dee45691e9d424a50f7ae2
iProdigy <prodigycreep@gmail.com>
2016-10-23 08:51:29 -0500

    read cancel properly

--- reserve.php
+++ reserve.php
@@ -4,8 +4,8 @@ require 'coc-helper.php';
 
 if (isset($_POST)) {
 	if (isset($_POST['userID']) && isset($_POST['cocID']) && isset($_POST['type']) && isset($_POST['cancel'])) {
-		$cancel = $_POST['cancel'];
-		if ($cancel === 'true') {
+		$cancel = json_decode($_POST['cancel']);
+		if ($cancel) {
 			return;
 		} else {
 			newReservation($_POST['userID'], $_POST['type'], $_POST['cocID']);
